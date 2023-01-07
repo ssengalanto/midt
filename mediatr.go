fa7294287b51edfceaf446a5bd1b1931be381769
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-01-07 15:22:35 +0800

    create mediatr package

---
+++ mediatr.go
@@ -0,0 +1,5 @@
+// Copyright 2023 Ssen Galanto. All rights reserved.
+
+// Package mediatr provides mediator pattern that reduces coupling between components
+// of a program by making them communicate indirectly, through a special mediator object.
+package mediatr
